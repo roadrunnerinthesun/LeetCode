b47271622d99be4e7b504e8f55389d2f0492804f
Sahara <saharabegom@gmail.com>
2022-05-23 23:11:33 +0000

    Solution to 20.Valid Parenthesis

--- LeetCode.py
+++ LeetCode.py
@@ -1,3 +1,5 @@
+# 20. Valid Parenthis
+
 """ Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
 determine if the input string is valid.
 An input string is valid if:
@@ -5,4 +7,30 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 """
 
-        
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Declare stack
+        stack = []
+        # If starting parentheses is encountered, then append it to the stack
+        for char in s:
+            if char in ['(' , '[' , '{']:
+                stack.append(char)
+        # If starting parentheses is not encountered, then return False
+            else:
+                if not stack: 
+                   return False
+            # Match pair of parentheses to pop out if end parentheses is encountered
+                current_char = stack.pop() 
+                if current_char == '(': 
+                    if char != ")": 
+                        return False
+                if current_char == '{': 
+                    if char != "}": 
+                        return False
+                if current_char == '[': 
+                    if char != "]": 
+                        return False
+        # If stack is empty
+        if stack: 
+            return False
+        return True
