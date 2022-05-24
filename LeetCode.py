6b1f5355437a64ef1c447880f76ee65702f83f54
Sahara <saharabegom@gmail.com>
2022-05-24 19:28:39 +0000

    Solution Complete, doctest added.

--- LeetCode.py
+++ LeetCode.py
@@ -34,3 +34,8 @@ class Solution:
         if stack: 
             return False
         return True
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print ("\n*** ALL TESTS PASSED. EXCELLENT JOB! ****\n")
