4cb38c9982ddcd954e5ba102094d7b5290afb37d
Sahara <saharabegom@gmail.com>
2022-05-26 17:54:54 +0000

    Two Sum Solution

--- Two_Sum.py
+++ Two_Sum.py
@@ -10,3 +10,9 @@ You can return the answer in any order.
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        values = {}
+        for idx, value in enumerate(nums):
+            if target - value in values:
+                return [values[target - value], idx]
+            else:
+                values[value] = idx
