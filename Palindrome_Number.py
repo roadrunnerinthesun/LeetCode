aa684c1b64a9da61d260e7574b5701a9999f12a2
Sahara <saharabegom@gmail.com>
2022-06-02 23:01:17 +0000

    Initial Commit Palindrome_Number

---
+++ Palindrome_Number.py
@@ -0,0 +1,8 @@
+# 9. Palindrome Number
+
+""" Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward.
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
